a0dbf9a2a76501fba625bdb5f758fbed536ea62a
daimiaopeng <32475719+daimiaopeng@users.noreply.github.com>
2019-12-07 09:47:49 +0800

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from model import Ithome
+import Ithome
 url = 'https://dyn.ithome.com/ithome/getajaxdata.aspx'  # IT之家评论信息请求地址
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
